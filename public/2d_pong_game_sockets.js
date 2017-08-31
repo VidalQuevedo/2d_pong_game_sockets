ca8cd72093cb29e6ad77f6c142c78b7a7f5c04fc
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2017-08-31 00:23:44 -0500

    add canvas

--- public/2d_pong_game_sockets.js
+++ public/2d_pong_game_sockets.js
@@ -1,3 +1,16 @@
 (function(){
-	console.log('hello');
+	
+	'use strict';
+
+	var socket = io();
+	var canvas = document.getElementById('2d-pong-game-sockets');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	init();
+
+	////////////////////
+
+	
+	
 })();
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = 3000;
 
+// Middleware
 app.use(express.static('public'));
 
+// Routes
 app.get('/', function(res, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+// Socket event handling
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+});
+
+// Start server
 http.listen(port, function(){
 	console.log('Listening in ' + port + '...');
 });
