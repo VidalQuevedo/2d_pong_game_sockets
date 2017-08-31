1b50ac0d94026cb4bd7e23ee4cbbb35602a844e5
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2017-08-31 01:31:28 -0500

    draw ball in more than one client at a time

--- public/2d_pong_game_sockets.js
+++ public/2d_pong_game_sockets.js
@@ -4,13 +4,44 @@
 
 	var socket = io();
 	var canvas = document.getElementById('2d-pong-game-sockets');
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	canvas.width = 500;
+	canvas.height = 500;
+
+	var state = {};
+	var ctx = canvas.getContext('2d');
 
 	init();
 
 	////////////////////
 
-	
+
+
+	function draw() {
+		clearCanvas();
+		drawBall();
+	}
+
+	function drawBall() {
+		ctx.beginPath();
+		ctx.arc(state.ballX, state.ballY, 10, 0, Math.PI * 2);
+		ctx.fillStyle = '#000000';
+		ctx.fill();
+		ctx.closePath();
+	}
+
+	function clearCanvas() {
+		ctx.clearRect(0, 0, state.canvasWidth, state.canvasHeight);	
+	}
+
+	function init() {
+		setSocketEventHandlers();
+	}
+
+	function setSocketEventHandlers() {
+		socket.on('refresh', function(data) {
+			state = data;
+			draw();
+		});
+	}
 	
 })();
--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 3000;
+var	state = {
+		ballX: 250,
+		ballY: 250,
+		ballRadius: 10,
+		canvasHeight: 500,
+		canvasWidth: 500,
+		dx: 2,
+		dy: -2
+	};
 
 // Middleware
 app.use(express.static('public'));
@@ -14,13 +23,38 @@ app.get('/', function(res, res) {
 
 // Socket event handling
 io.on('connection', function(socket) {
-	console.log('a user connected');
-	socket.on('disconnect', function() {
-		console.log('user disconnected');
+	io.clients(function(error, clients) {
+		if (error) throw error;
+		console.log(clients.length);
+		if (clients.length === 1) {
+			setInterval(function(){
+				moveBall();
+				ballCollisionDetection();
+				io.emit('refresh', state);
+				console.log(state);
+			}, 10);	
+		}
 	});
 });
+
+
 
 // Start server
 http.listen(port, function(){
 	console.log('Listening in ' + port + '...');
 });
+
+
+////////////////////
+
+function ballCollisionDetection() {
+	// ceiling and floor
+	if (state.ballY - state.ballRadius < 0 || state.ballY + state.ballRadius > state.canvasHeight) {
+		state.dy = -state.dy;
+	}
+}
+
+function moveBall() {
+	// state.ballX += state.dx;
+	state.ballY += state.dy;
+}
